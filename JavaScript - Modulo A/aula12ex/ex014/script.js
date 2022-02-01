40fae20aafa51669826155e9ecf8afd5340d10af
Vkpn <victoraso10@hotmail.com>
2022-02-01 00:44:41 -0300

    update de exercicios

---
+++ JavaScript - Modulo A/aula12ex/ex014/script.js
@@ -0,0 +1,26 @@
+function carregar() {
+    var msg = window.document.getElementById(`msg`)
+    var img = window.document.getElementById(`imagem`)
+    var data = new Date()
+    //var hora = data.getHours()
+
+    var hora= 
+
+    if (hora >= 6 && hora < 12){
+        msg.innerHTML = `Bom dia! Agora são ${hora} horas.`
+        img.src = `imagens/manha.png`
+        document.body.style.background = `#66944b` 
+    } else if (hora >= 12 && hora < 18){
+        msg.innerHTML = `Boa tarde! Agora são ${hora} horas.`
+        img.src = `imagens/tarde.png`
+        document.body.style.background = `#c2937e`
+    } else if (hora >= 18 && hora < 24){
+        msg.innerHTML = `Boa noite! Agora são ${hora} horas.`
+        img.src = `imagens/noite.png`
+        document.body.style.background = `#654f8f`
+    } else {
+        msg.innerHTML = `Boa madrugada! Agora são ${hora} horas.`
+        img.src = `imagens/madrugada.png`
+        document.body.style.background = `#696969`
+    }
+}
