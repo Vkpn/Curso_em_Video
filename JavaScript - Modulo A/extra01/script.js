f14a6d66a361ce85b30cdaa97fda173dae2e5892
Vkpn <victoraso10@hotmail.com>
2022-02-02 02:32:20 -0300

    Update script.js

--- JavaScript - Modulo A/extra01/script.js
+++ JavaScript - Modulo A/extra01/script.js
@@ -5,12 +5,15 @@ function ver(){
     var sel = document.getElementsByName(`estilo`)
     var res = document.getElementById(`res`)
 
+    var email = String(txtemail.value)
+
     var data = new Date()
     var ano = data.getFullYear()
 
     if (nome.value == 0 || fano.value == 0 || fano.value >= ano || mail.value == 0){
         window.alert(`[ERRO] Preencha corretamente os dados!`)
     } else {
+        
         var nome = String(txtname.value)
         var idade = ano - Number(fano.value)
         var game = ``
@@ -37,7 +40,7 @@ function ver(){
         res.style.textAlign = `center`
         res.innerHTML = `<p>Olá ${nome}, vemos que você é um gamer de ${idade} ${anomsg} e gosta de jogar ${game} nas suas horas vagas.</p>`
         res.appendChild(img)
+        window.alert(`Muito obrigado por visitar minha página!
+[Usuário: ${email}]`)
     }
-
-    
 }
